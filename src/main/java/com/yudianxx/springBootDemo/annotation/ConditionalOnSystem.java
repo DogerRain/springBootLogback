4db3421b7536a5955ffbd8d2f3eb7a601083f3c4
Administrator <huangyongwen0306@163.com>
2019-08-04 23:51:39 +0800

    1.最长无重复字符串 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/

--- src/main/java/com/yudianxx/springBootDemo/annotation/ConditionalOnSystem.java
+++ src/main/java/com/yudianxx/springBootDemo/annotation/ConditionalOnSystem.java
@@ -9,11 +9,11 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
+//当调用system的时候，就会去加载这个OnSystemCondition.class这个类，假如不止一个system参数呢，估计应该也是会加载这个类
 @Conditional({OnSystemCondition.class})
 public @interface ConditionalOnSystem {
 
     String system();
 
-    Boolean flag=false;
 
 }
