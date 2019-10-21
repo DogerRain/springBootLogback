8e773edbb2f9952439e3c01123f7bc2e7ba6b531
Administrator <huangyongwen0306@163.com>
2019-10-21 16:17:59 +0800

    1多态
    Signed-off-by: Administrator <huangyongwen0306@163.com>

--- src/main/java/com/yudianxx/basic/Abstract/多态.java
+++ src/main/java/com/yudianxx/basic/Abstract/多态.java
@@ -1,6 +1,28 @@
 package com.yudianxx.basic.Abstract;
 
-public class A1 {
+public class 多态 {
+    public static void main(String[] args) {
+        A1 a = new A1();
+        B1 b = new B1();
+        C1 c = new C1();
+
+        A1 a1 = new B1();
+        a1.menthod_one();
+        a = (A1) b;  //     相当于  A1  a = new B1(); 向上转型,通过父类实例化子类，指向了子类的引用
+        a.menthod_one();
+
+
+        B1 bb = (B1) a; //向下转型
+        bb.menthod_one();
+
+        A1 ac = new C2();
+        ac.menthod_one();
+
+    }
+}
+
+
+class A1 {
     //    java提供的两种多态的机制：
 //    父类或接口定义的引用变量可以指向子类或具体实现类的实例对象，
 //    而程序调用的方法在运行期才动态绑定，就是引用变量所指向的具体
@@ -16,24 +38,10 @@ public class A1 {
 
 
 class B1 extends A1 {
-//    public void menthod_one() {
-//        System.out.println("B");
-//    }
-
-
-    public static void main(String[] args) {
-        A1 a = new A1();
-        B1 b = new B1();
-        C1 c = new C1();
-
-        A1  a1 = new B1();
-        a1.menthod_one();
-//        a = (A1) b;  //     相当于  A1  a = new B1(); 向下转型,通过父类实例化子类，指向了子类的引用
-        c.menthod_one();
-//        c = (C1) a;
-//        c.menthod_one();
-
+    public void menthod_one() {
+        System.out.println("B");
     }
+
 
 }
 
@@ -43,6 +51,10 @@ class C1 extends A1 {
     }
 }
 
-
+class C2 extends C1 {
+    public void menthod_one() {
+        System.out.println("C2");
+    }
+}
 
 
